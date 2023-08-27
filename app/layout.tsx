7efaba92c0c2737a8458b2cd24f2f6a334fbc1e6
pratokko <pratokko3@gmail.com>
2023-08-27 15:25:31 +0300

    feat:  dbconnect

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={ cn(font.className, "bg-white dark:bg-[#313338]") }>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
