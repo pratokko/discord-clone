cd4e9ce4405154c7fcb3cac8be080f31df12a058
pratokko <pratokko3@gmail.com>
2023-08-30 14:01:49 +0300

    feat:  invite functionality added

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className=" md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+      <div className="md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar />
       </div>
 
